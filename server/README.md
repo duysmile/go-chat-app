7e41a4c9ba92bc5062ae4348780d08448e6e79a5
Duy Nguyen <duy210697@gmail.com>
2022-01-27 11:04:10 +0700

    [dev] init with too much pain

--- server/README.md
+++ server/README.md
@@ -1,3 +1,32 @@
-# Go chat server
+# Go Chat App - Text + Voice
 
-This project build a chat server in Go using websocket
+### Step by step
+1. Download package for gRPC:
+```bash
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+```
+
+2. Install protoc
+```bash
+brew install protobuf
+```
+
+3. Init go module
+```bash
+go mod init github.com/duysmile/go-chat-app
+```
+
+4. Create Makefile to automate your work:
+- generate: generate proto to go protobuf
+
+5. Install packages:
+```bash
+go get google.golang.org/grpc
+go get github.com/grpc-ecosystem/grpc-gateway
+```
+
+6. Build simple server with gRPC
+- Follow this tutorial: https://grpc-ecosystem.github.io/grpc-gateway/docs/tutorials/adding_annotations/
+
--- server/main.go
+++ server/main.go
@@ -1,43 +1,70 @@
 package main
 
 import (
-	"fmt"
-	"go-chat-app/pkg/websocket"
+	"context"
+	"log"
+	"net"
 	"net/http"
+
+	"github.com/duysmile/gochatapp/pb"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Socket connected from:", r.Host)
-
-	conn, err := websocket.Upgrade(w, r)
-	if err != nil {
-		fmt.Fprintf(w, "%+V\n", err)
-		return
-	}
-
-	client := &websocket.Client{
-		Conn: conn,
-		Pool: pool,
-	}
-
-	pool.Register <- client
-	client.Read()
+type server struct {
+	pb.UnimplementedGreeterServer
 }
 
-func setupRoutes() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(rw, "Simple server")
-	})
+func NewServer() *server {
+	return &server{}
+}
 
-	pool := websocket.NewPool()
-	go pool.Start()
-	http.HandleFunc("/ws", func(rw http.ResponseWriter, r *http.Request) {
-		serveWs(pool, rw, r)
-	})
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: in.Name + " world"}, nil
 }
 
 func main() {
-	fmt.Println("Chat App Server")
-	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+	// Attach the Greeter service to the server
+	pb.RegisterGreeterServer(s, &server{})
+	// Serve gRPC server
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		"0.0.0.0:8080",
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = pb.RegisterGreeterHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("Serving gRPC-Gateway on http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
 }
